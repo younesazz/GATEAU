815c551b2f0009f4f96a7f67616c4dcde105894a
Hamani_Younes <68382115+younesazz@users.noreply.github.com>
2022-08-16 17:51:36 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
       <p className="icons">
         <a href='https://www.facebook.com/Lee-g%C3%A2teau-parfait-103101619173835' target='_blank'><FaFacebook /></a>
         <a href='https://www.instagram.com/direct/t/340282366841710300949128194709864694422' target='_blank'><AiFillInstagram/></a>
-        <a href='tel:+213 0697600642' target='_blank'><AiFillPhone/></a>
+        <a href='tel:+213 0666661899' target='_blank'><AiFillPhone/></a>
       </p>
     </div>
   )
